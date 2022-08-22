8d40543471a87ca6e950c03582dc1152c2a21395
DevDoge <lhuthefranc@hotmail.com>
2022-08-22 18:04:09 -0300

    api/ route added

--- src/main/java/com/example/springbootbackend/controller/EmployeeController.java
+++ src/main/java/com/example/springbootbackend/controller/EmployeeController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/")
 public class EmployeeController {
 
     @Autowired
